d5c2304ddff5eb69cb92a73adb5d8788b70976d9
kim uhjin <ajin0714@imagoworks.ai>
2021-05-26 14:55:53 +0900

    modify test

--- test.js
+++ test.js
@@ -2,8 +2,7 @@ import { AbortController } from '@azure/abort-controller';
 import { BlobServiceClient, BlockBlobClient } from '@azure/storage-blob';
 import { CancelablePromise } from 'cancelable-promise';
 let url = 'https://datagarage.blob.core.windows.net';
-let token =
-    'BlobEndpoint=https://datagarage.blob.core.windows.net/;QueueEndpoint=https://datagarage.queue.core.windows.net/;FileEndpoint=https://datagarage.file.core.windows.net/;TableEndpoint=https://datagarage.table.core.windows.net/;SharedAccessSignature=sv=2020-02-10&ss=bfqt&srt=sco&sp=rwdlacupx&se=2022-05-12T17:30:08Z&st=2021-05-12T09:30:08Z&spr=https&sig=I2o96ftPYk6epMPtL2i6ndt3M7wQdoOHL29kdEFApaw%3D';
+let token = null;
 let container = 'files';
 let root = 'test';
 
@@ -52,7 +51,6 @@ const handleUploadFiles = async (Files, root, format, fileSize) => {
     };
 
     const fileUpload = async (container, root, file, token) => {
-        console.log(file);
         try {
             const getBlobInfoPromise = uploadBlobAs(
                 container,
@@ -85,13 +83,13 @@ const getBlobListfromContainer = async (url, token, container) => {
     const containerClient = await blobServiceClient.getContainerClient(
         container
     );
-    const getListBolbs = async () => {
+    const getListBlobs = async () => {
         for await (const blob of containerClient.listBlobsFlat()) {
             blob.downloadPath = `${url}/${container}/${blob.name}`;
             tempBlobLists.push(blob);
         }
     };
-    await getListBolbs();
+    await getListBlobs();
     return tempBlobLists;
 };
 const deleteFile = async (file) => {
@@ -126,55 +124,136 @@ const modifyFile = async (oldFileName, newFileName) => {
         return newBlobClient;
     }
 };
-test('파일 업로드 확인', async () => {
-    let Files = new ArrayBuffer(8);
-    Files.name = 'test.zip';
-    Files.size = 999;
-    let fileSize = 10000;
-    let format = ['zip', 'pdf', '7z', 'txt', 'jpg'];
-    await handleUploadFiles([Files], root, format, fileSize);
-    setTimeout(async () => {
-        const blobList = await getBlobListfromContainer(url, token, container);
-        let tempBlobList = ['234'];
-        console.log(blobList);
-        await blobList.map((file) => {
-            tempBlobList.push(file.name);
-        });
-        console.log(tempBlobList);
-        await expect(tempBlobList).toContain('test/test.zip');
-    }, 1000);
-});
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
 
-test('파일명 수정 확인', async () => {
-    await modifyFile('test/test.zip', 'test/test_new.zip');
-    const blobList = await getBlobListfromContainer(url, token, container);
-    let tempBlobList = [];
-    blobList.map((file) => {
-        tempBlobList.push(file.name);
+describe('파일 업로드 및 확인', () => {
+    test('파일 업로드', async (done) => {
+        let Files = new ArrayBuffer(8);
+        Files.name = 'test.zip';
+        Files.size = 999;
+        let fileSize = 10000;
+        let format = ['zip', 'pdf', '7z', 'txt', 'jpg'];
+
+        const handleUploadFilesAsync = async () => {
+            await handleUploadFiles([Files], root, format, fileSize);
+            done();
+        };
+        await handleUploadFilesAsync();
+        done();
     });
-    expect(tempBlobList).toContain('test/test_new.zip');
-});
 
-test('폴더 이동 확인', async () => {
-    await modifyFile('test/test_new.zip', 'test_new/test_new.zip');
-    const blobList = await getBlobListfromContainer(url, token, container);
-    let tempBlobList = [];
-    blobList.map((file) => {
-        tempBlobList.push(file.name);
+    test('파일 업로드 확인', (done) => {
+        setTimeout(async () => {
+            let tempBlobList = [];
+            const handleBlobList = async () => {
+                const blobList = await getBlobListfromContainer(
+                    url,
+                    token,
+                    container
+                );
+
+                for (let file of blobList) {
+                    tempBlobList.push(file.name);
+                }
+            };
+
+            await handleBlobList();
+            expect(tempBlobList).toContain('test/test.zip');
+            done();
+        }, 3000);
     });
-    expect(tempBlobList).toContain('test_new/test_new.zip');
+});
+describe('파일 수정 및 확인', () => {
+    test('파일 수정', async (done) => {
+        const handleModifyFile = async () => {
+            await modifyFile('test/test.zip', 'test/test_new.zip');
+            done();
+        };
+        await handleModifyFile();
+        done();
+    });
+    test('파일 업로드 확인', (done) => {
+        setTimeout(async () => {
+            let tempBlobList = [];
+            const handleBlobList = async () => {
+                const blobList = await getBlobListfromContainer(
+                    url,
+                    token,
+                    container
+                );
+
+                for (let file of blobList) {
+                    tempBlobList.push(file.name);
+                }
+            };
+
+            await handleBlobList();
+            expect(tempBlobList).toContain('test/test_new.zip');
+            done();
+        }, 3000);
+    });
 });
 
-test('파일 삭제 확인', async () => {
-    await deleteFile('test_new/test_new.zip');
-    setTimeout(async () => {
-        const blobList = await getBlobListfromContainer(url, token, container);
-        let tempBlobList = [];
-        blobList.map((file) => {
-            tempBlobList.push(file.name);
-        });
-        await expect(
-            tempBlobList.some((file) => file === 'test_new/test_new.zip')
-        ).toBe(false);
-    }, 1000);
+describe('폴더 이동 및 확인', () => {
+    test('폴더 이동', async (done) => {
+        const handleModifyFile = async () => {
+            await modifyFile('test/test_new.zip', 'test_new/test_new.zip');
+            done();
+        };
+        await handleModifyFile();
+        done();
+    });
+    test('파일 업로드 확인', (done) => {
+        setTimeout(async () => {
+            let tempBlobList = [];
+            const handleBlobList = async () => {
+                const blobList = await getBlobListfromContainer(
+                    url,
+                    token,
+                    container
+                );
+
+                for (let file of blobList) {
+                    tempBlobList.push(file.name);
+                }
+            };
+
+            await handleBlobList();
+            expect(tempBlobList).toContain('test_new/test_new.zip');
+            done();
+        }, 3000);
+    });
+});
+
+describe('파일 삭제 및 확인', () => {
+    test('폴더 이동', async (done) => {
+        const handleDeleteFile = async () => {
+            await deleteFile('test_new/test_new.zip');
+            done();
+        };
+        await handleDeleteFile();
+        done();
+    });
+    test('파일 업로드 확인', (done) => {
+        setTimeout(async () => {
+            let tempBlobList = [];
+            const handleBlobList = async () => {
+                const blobList = await getBlobListfromContainer(
+                    url,
+                    token,
+                    container
+                );
+
+                for (let file of blobList) {
+                    tempBlobList.push(file.name);
+                }
+            };
+
+            await handleBlobList();
+            expect(
+                tempBlobList.some((file) => file === 'test_new/test_new.zip')
+            ).toBe(false);
+            done();
+        }, 3000);
+    });
 });
